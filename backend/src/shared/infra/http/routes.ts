09a1715a0a7ec5ef08bcc916b0726524b1eab28c
Rafael Luiz <luizrafael391@gmail.com>
2022-06-18 13:20:30 -0300

    refactor: add user.routes to routes

--- backend/src/shared/infra/http/routes.ts
+++ backend/src/shared/infra/http/routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import userRoutes from '../../../modules/user/infra/routes';
+import userRoutes from '../../../modules/user/infra/user.routes';
 
 const router = Router();
 
