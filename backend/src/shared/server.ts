0af18b1716f5e182a2c1f4a5d51d4160824b6845
Rafael Luiz <luizrafael391@gmail.com>
2022-06-19 14:54:55 -0300

    refactor: add port env variable on server

--- backend/src/shared/server.ts
+++ backend/src/shared/server.ts
@@ -25,6 +25,8 @@ server.use((err: Error, _: Request, res: Response, __: any) => {
   return res.status(500).json(err);
 });
 
-server.listen(3000, () => {
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
   console.log('test');
 });
